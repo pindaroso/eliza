5de8e5ea3873ebaf035bde528895f912b0e1bdaa
me@pindaroso.com <me@oberlinstands.com>
2024-11-15 12:24:57 -0500

    Updated .env

--- scripts/ship.sh
+++ scripts/ship.sh
@@ -5,14 +5,7 @@ set -e
 # Source the .env file
 source .env
 
-BLUE=/home/ubuntu/squid/blue
 GREEN=/home/ubuntu/squid/green
-TEMP=/home/ubuntu/squid/temp
 
-scp -i $PEM -r characters .env docker-compose.yml $SERVER:$TEMP
-
-ssh -i $PEM $SERVER "mkdir -p $GREEN"
-ssh -i $PEM $SERVER "rm -rf $BLUE"
-ssh -i $PEM $SERVER "mv $GREEN $BLUE && mv $TEMP $GREEN"
-
-ssh -i $PEM $SERVER "cd $GREEN && docker compose up -d --remove-orphans && docker compose restart"
+scp -i $PEM -r characters .env nAIssim_taleb.env docker-compose.yml $SERVER:$GREEN
+ssh -i $PEM $SERVER "cd $GREEN && docker compose pull && docker compose up -d --remove-orphans && docker compose restart"
